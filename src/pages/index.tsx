a2919f019ce191ffe992798c01e2d5fb9c2b09c4
Rodrigo Almeida de Magalhaes <rodrigo.a.magalhaes@hotmail.com>
2021-03-24 23:38:04 -0300

    modulo 1 - video aula

---
+++ src/pages/index.tsx
@@ -0,0 +1,57 @@
+import { GetStaticProps } from 'next';
+
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stipe';
+import sytles from './home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={sytles.contentContainer}>
+        <section className={sytles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about <br /> the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1IYhx2AwzOn5g5KbkmYd14RN');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24 horas
+  }
+}
