b8ad6dccba32be9207d20ccb0554d1f1bd5eb6af
Rodrigo Almeida de Magalhaes <rodrigo.a.magalhaes@hotmail.com>
2021-03-27 00:51:55 -0300

    modulo api FaunaDB

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
-import { stripe } from '../services/stipe';
+import { stripe } from '../services/stripe';
 import sytles from './home.module.scss';
 
 interface HomeProps {
